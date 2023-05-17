8024d68e6842f2ae4d5823d11c4b4d7696d98cc2
BabaRose <talhakaragul@marun.edu.tr>
2023-05-17 20:32:26 +0300

    a

--- boyerMoore.js
+++ boyerMoore.js
@@ -1,9 +1,8 @@
 // Time complexity: O(mn)
 // Space complexity: O(m + n)
 function boyerMoore(text, pattern) {
-    // create the bad match table
-    const badSymbolTable = {};
     const patternLength = pattern.length;
+    // console.log(patternLength); 
     const textLength = text.length;
     const indexes = [];
     const badSymbolTable = {};
@@ -14,26 +13,38 @@ function boyerMoore(text, pattern) {
 
     // create bad symbol table
     for (let i = 0; i < patternLength; i++) {
-        badSymbolTable[pattern[i]] = patternLength - i - 1;
+        badSymbolTable[pattern[i]] = Math.max(1, patternLength - i - 1);
+    
     }
     badSymbolTable['*'] = patternLength;
 
-    // create good suffix table
-    for (let i = 0; i < patternLength; i++) {
-        badSymbolTable[pattern[i]] = Math.max(1, patternLength - i - 1);  
-    }
-    badSymbolTable['*'] = patternLength;
 
-    let j = 0;
-    for (let i = patternLength - 1; i >= 0; i--) {
-        if (goodSuffixTable[i] === i + 1) {
-            for (; j < patternLength - 1 - i; j++) {
-                if (goodSuffixTable[j] === patternLength) {
-                    goodSuffixTable[j] = patternLength - 1 - i;
-                }
-            }
+   
+// create good suffix table
+for (let i = 1; i < patternLength; i++) {
+    goodSuffixTable[i] = patternLength;
+}
+
+for (let i = patternLength - 1; i >= 1; i--) {
+    let sub = pattern.substring(i, patternLength + 1);
+    let isAllSame = true;
+    for (let j = 0; j < sub.length; j++) {
+        const tempChar = sub[sub.length - 1 - j];
+        const compareText = pattern.substring(0, patternLength - sub.length - j);
+        if (compareText === '') {
+            break;
         }
+        if (tempChar !== compareText[compareText.length - 1]) {
+            isAllSame = false;
+            break;
+        }            
     }
+    if (isAllSame) {
+        goodSuffixTable[i] = patternLength - sub.length;
+        }
+    }
+
+
 
     // search the pattern in the text
     let i = patternLength - 1;
@@ -49,11 +60,12 @@ function boyerMoore(text, pattern) {
             indexes.push(i - patternLength + 1);
         }
         comparisons++;
-        const badMatchShift = badSymbolTable[text[i]];
+         const badMatchShift = badSymbolTable[text[i]];
         const goodSuffixShift = goodSuffixTable[k - 1];
         i += Math.max(badMatchShift || 0, goodSuffixShift);
     }
-    return { badSymbolTable, goodSuffixTable, indexes, comparisons, occurrences };
+
+    return { badSymbolTable,goodSuffixTable, indexes, comparisons, occurrences };
 }
-// boyerMoore("languageaasfdasdghs", "language")
+// boyerMoore("111111111111111000000000111112111111111111010101", "111111111111111")
 module.exports = { boyerMoore };
