d1f4949f70fe86acd721015504203052a8934430
BabaRose <talhakaragul@marun.edu.tr>
2023-05-17 17:04:59 +0300

    boyer e update

--- boyerMoore.js
+++ boyerMoore.js
@@ -1,23 +1,41 @@
 // Time complexity: O(mn)
 // Space complexity: O(m + n)
 function boyerMoore(text, pattern) {
-    // create the bad match table
     const badMatchTable = {};
     const patternLength = pattern.length;
     const textLength = text.length;
     const indexes = [];
+    const badSymbolTable = {};
+    const goodSuffixTable = {};
 
     let comparisons = 0;
     let occurrences = 0;
-    // create the bad match table
-    for (let i = 0; i < patternLength; i++) {
-        badMatchTable[pattern[i]] = Math.max(1, patternLength - i - 1);  
-    }
-    badMatchTable['*'] = patternLength;
 
-    //---------------------------------------------
-    // good suffix table yok oluşturulması gerekiyor 
-    //---------------------------------------------
+    // create bad symbol table
+    for (let i = 0; i < patternLength; i++) {
+        badSymbolTable[pattern[i]] = patternLength - i - 1;
+    }
+    badSymbolTable['*'] = patternLength;
+
+
+   
+    // create good suffix table
+    for (let i = 0; i < patternLength; i++) {
+        goodSuffixTable[i] = patternLength;
+    }
+
+    let j = 0;
+    for (let i = patternLength - 1; i >= 0; i--) {
+        if (goodSuffixTable[i] === i + 1) {
+            for (; j < patternLength - 1 - i; j++) {
+                if (goodSuffixTable[j] === patternLength) {
+                    goodSuffixTable[j] = patternLength - 1 - i;
+                }
+            }
+        }
+    }
+
+
 
 
     // search the pattern in the text
@@ -34,10 +52,12 @@ function boyerMoore(text, pattern) {
             indexes.push(i - patternLength + 1);
         }
         comparisons++;
-        i += badMatchTable[text[i]] || patternLength;
+         const badMatchShift = badSymbolTable[text[i]];
+        const goodSuffixShift = goodSuffixTable[k - 1];
+        i += Math.max(badMatchShift || 0, goodSuffixShift);
     }
 
-    return { badMatchTable, indexes, comparisons, occurrences };
+    return { badSymbolTable,goodSuffixTable, indexes, comparisons, occurrences };
 }
 // boyerMoore("languageaasfdasdghs", "language")
 module.exports = { boyerMoore };
